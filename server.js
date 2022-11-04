20bcaf018d160490eca20e4e75e393ac0860334b
Temitope Agbaje <agbajeinioluwa@gmail.com>
2022-11-04 02:27:07 +0100

    update

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const PORT = process.env.PORT || 4000;
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get("/", async (req, res, next) => {
+  return res.status(200).send({
+    slackUsername: "Temitope Agbaje",
+    backend: true,
+    age: 23,
+    bio: "I am a very friendly and cheerful person",
+  });
+});
+
+app.post("/", async (req, res) => {
+  const operation_type = ["addition", "subtraction", "multiplication"];
+  const data = {
+    x: req.body.x,
+    y: req.body.y,
+    operation_type,
+  };
+
+  result = data.x + data.y;
+
+  return res.status(200).contentType('application/json').send({
+    slackUsername: "Temitope Agbaje",
+    operation_type: data.operation_type[0],
+    result: result,
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
